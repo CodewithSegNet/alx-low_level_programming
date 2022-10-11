6c856d5e09da9a92391006d0be01d86e08a638ed
Omogbehinmi Olusegun Emmanuel <eeomo@yahoo.com>
2022-10-10 19:10:53 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,6 @@
-#ifndef FUNCTION_LIKE_MACRO
-#define FUNCTION_LIKE_MACRO
-#define ABS(x) (((x) < 0) ? (x * -1) : (x))
-#endif
+#ifndef _function_like_macro_h_
+#define _function_like_macro_h_
+
+#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
+
+#endif /* _function_like_macro_h_ */
